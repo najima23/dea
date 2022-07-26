64f2c1ebf58c02625c17f744636d77af6c994d35
Najima Jaadari <najimajaadari@Najimas-MBP-2.fritz.box>
2022-07-26 22:01:06 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,24 @@ import { MatButtonModule } from '@angular/material/button';
 import { DeaModule } from './dea/dea.module';
 import { DialogComponent } from './shared/dialog/dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'your-realm',
+        clientId: 'your-client-id'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 const appRoutes: Routes = [
   { path: '', component: MainPageComponent },
@@ -43,6 +61,7 @@ const appRoutes: Routes = [
     MatButtonModule,
     DeaModule,
     MatDialogModule,
+    KeycloakAngularModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
