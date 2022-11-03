bd7a126d814c0c03c71c7e0a65383f670e5f24b5
Najima Jaadari <najimajaadari@Najimas-MBP-2.fritz.box>
2022-11-03 10:33:27 +0100

    Musterlösung entfernt

--- src/app/puzzle-game/level.ts
+++ src/app/puzzle-game/level.ts
@@ -9,18 +9,8 @@ export const game = {
   },
   1: {
     task: 'Stellen Sie die nachfolgenden Sprache als deterministische endliche Automaten dar \n L = {w | w ∈ {0,1}∗ der nur Wörter akzeptiert, die mit 0 anfangen}',
-    nodes: [
-      { id: 0, text: "A" },
-      { id: 1, text: "B", figure: 'Ring' },
-      { id: 2, text: "C" }
-
-    ],
-    links: [
-      { key: 0, from: 0, to: 1, text: '0' },
-      { key: 1, from: 0, to: 2, text: '1' },
-      { key: 2, from: 2, to: 2, text: '0,1' },
-      { key: 4, from: 1, to: 1, text: '0,1' },
-    ],
+    nodes: [],
+    links: [],
     rightWords: function () {
       const wordGenerator = new WordGenerator();
       const words = wordGenerator.generateWords({
@@ -63,18 +53,8 @@ export const game = {
   },
   2: {
     task: 'Stellen Sie die nachfolgenden Sprache als deterministische endliche Automaten dar \n L = {w | w ∈ {0,1}∗ und an jeder ungerade Position von w eine 1 steht}',
-    nodes: [
-      { id: 0, text: "A", figure: 'Ring' },
-      { id: 1, text: "B", figure: 'Ring' },
-      { id: 2, text: "C" }
-    ],
-    links: [
-      { key: 0, from: 0, to: 1, text: '1' },
-      { key: 1, from: 1, to: 0, text: '0,1' },
-      { key: 2, from: 0, to: 2, text: '0' },
-      { key: 3, from: 2, to: 2, text: '0,1' }
-
-    ],
+    nodes: [],
+    links: [],
     rightWords: function () {
       const wordGenerator = new WordGenerator();
       const words = wordGenerator.generateWords({
@@ -117,21 +97,8 @@ export const game = {
   },
   3: {
     task: 'Stellen Sie die nachfolgenden Sprache als deterministische endliche Automaten dar \n L = {w | w ∈ {0,1}∗ und w enthaelt das Teilwort 110}',
-    nodes: [
-      { id: 0, text: "A" },
-      { id: 1, text: "B" },
-      { id: 2, text: "C" },
-      { id: 3, text: "D", figure: 'Ring' }
-    ],
-    links: [
-      { key: 0, from: 0, to: 1, text: '1' },
-      { key: 1, from: 0, to: 0, text: '0' },
-      { key: 2, from: 1, to: 0, text: '0' },
-      { key: 3, from: 1, to: 2, text: '1' },
-      { key: 4, from: 2, to: 2, text: '1' },
-      { key: 5, from: 2, to: 3, text: '0' },
-      { key: 6, from: 3, to: 3, text: '0,1' }
-    ],
+    nodes: [],
+    links: [],
     rightWords: function () {
       const wordGenerator = new WordGenerator();
       const words = wordGenerator.generateWords({
@@ -182,19 +149,8 @@ export const game = {
   },
   4: {
     task: 'Stellen Sie die nachfolgenden Sprache als deterministischen endlichen Automaten dar L = {w | w ∈ {0,1}}∗ und w hat eine Länge von maximal 2',
-    nodes: [
-      { id: 0, text: "A", },
-      { id: 1, text: "B", },
-      { id: 2, text: "C", figure: 'Ring' },
-      { id: 3, text: "D" }
-    ],
-    links: [
-      { key: "Start", from: -1, to: 0, text: 'Start' },
-      { key: 0, from: 0, to: 1, text: '0,1' },
-      { key: 1, from: 1, to: 2, text: '0,1' },
-      { key: 2, from: 2, to: 3, text: '0,1' },
-      { key: 3, from: 3, to: 3, text: '0,1' },
-    ],
+    nodes: [],
+    links: [],
     solution: {
       nodes: [
         { id: 0, text: "A", },
@@ -220,16 +176,8 @@ export const game = {
   },
   5: {
     task: '̈Gegeben sei die Sprache L1 = {w ∈ {0} *| |w| ist durch 3 teilbar}. Geben Sie einen deterministischen endlichen Automaten in Graphdarstellung an, der L1 erkennt',
-    nodes: [
-      { id: 0, text: "A", color: "white", figure: 'Ring' },
-      { id: 1, text: "B" },
-      { id: 2, text: "C" },
-    ],
-    links: [
-      { key: 0, from: 0, to: 1, text: '0' },
-      { key: 1, from: 1, to: 2, text: '0' },
-      { key: 2, from: 2, to: 0, text: '0' },
-    ],
+    nodes: [],
+    links: [],
     solution: {
       nodes: [
         { id: 0, text: "A", color: "white", figure: 'Ring' },
@@ -253,21 +201,13 @@ export const game = {
   },
   6: {
     task: '̈Gegeben sei die Sprache L = {w ∈ {0} * : |w| ≥ 2 }. Geben Sie einen deterministischen endlichen Automaten in Graphdarstellung an, der L erkennt',
-    nodes: [
-      { id: 0, text: "A"},
-      { id: 1, text: "B" },
-      { id: 2, text: "C", figure: 'Ring'},
-    ],
-    links: [
-      { key: 0, from: 0, to: 1, text: '0,1' },
-      { key: 1, from: 1, to: 2, text: '0,1' },
-      { key: 2, from: 2, to: 2, text: '0,1' },
-    ],
+    nodes: [],
+    links: [],
     solution: {
       nodes: [
-        { id: 0, text: "A"},
+        { id: 0, text: "A" },
         { id: 1, text: "B" },
-        { id: 2, text: "C", figure: 'Ring'},
+        { id: 2, text: "C", figure: 'Ring' },
       ],
       links: [
         { key: "Start", from: -1, to: 0, text: 'Start' },
@@ -286,24 +226,14 @@ export const game = {
   },
   7: {
     task: '̈Gegeben sei die Sprache L = {w ∈ {0} * : |w| ≤ 2 }. Geben Sie einen deterministischen endlichen Automaten in Graphdarstellung an, der L erkennt',
-    nodes: [
-      { id: 0, text: "A", figure: 'Ring'},
-      { id: 1, text: "B", figure: 'Ring'},
-      { id: 2, text: "C", figure: 'Ring'},
-      { id: 3, text: "D"}
-    ],
-    links: [
-      { key: 0, from: 0, to: 1, text: '0,1' },
-      { key: 1, from: 1, to: 2, text: '0,1' },
-      { key: 2, from: 2, to: 3, text: '0,1' },
-      { key: 3, from: 3, to: 3, text: '0,1' }
-    ],
+    nodes: [],
+    links: [],
     solution: {
       nodes: [
-        { id: 0, text: "A", figure: 'Ring'},
-        { id: 1, text: "B", figure: 'Ring'},
-        { id: 2, text: "C", figure: 'Ring'},
-        { id: 3, text: "D"}
+        { id: 0, text: "A", figure: 'Ring' },
+        { id: 1, text: "B", figure: 'Ring' },
+        { id: 2, text: "C", figure: 'Ring' },
+        { id: 3, text: "D" }
       ],
       links: [
         { key: "Start", from: -1, to: 0, text: 'Start' },
